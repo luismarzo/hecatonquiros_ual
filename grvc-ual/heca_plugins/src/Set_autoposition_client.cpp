ca52cedca7ab0e1f70ec0ad5d48efba7649c96ac
Luis Marzo <luismarzor@gmail.com>
2018-10-04 16:00:35 +0200

    uav flies dron

---
+++ grvc-ual/heca_plugins/src/Set_autoposition_client.cpp
@@ -0,0 +1,40 @@
+#include "ros/ros.h"
+#include "heca_plugins/Set_position.h"
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "Set_autoposition_client");
+  ROS_INFO("argc:%d\n",argc);
+  if (argc != 1)
+  {
+    ROS_INFO("Error, too much arguments");
+    return 1;
+  }
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<heca_plugins::Set_position>("Set_autoposition");
+  heca_plugins::Set_position srv;
+  /*srv.request.x = atoll(argv[1]);
+  srv.request.y = atoll(argv[2]);
+  srv.request.z = atoll(argv[3]);
+  srv.request.roll = atoll(argv[4]);
+  srv.request.pitch = atoll(argv[5]);
+  srv.request.yaw = atoll(argv[6]);*/
+  if (client.call(srv))
+  {
+    ROS_INFO("Pos: [%ld,%ld,%ld,%ld,%ld,%ld]", (long int)srv.response.resu1,
+						(long int)srv.response.resu2,
+						(long int)srv.response.resu3,
+						(long int)srv.response.resu4,
+						(long int)srv.response.resu5,
+						(long int)srv.response.resu6);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service ");
+    return 1;
+  }
+
+  return 0;
+}
---
+++ grvc-ual/heca_plugins/src/positioner_plugin.cpp
@@ -0,0 +1,161 @@
+#include "boost/bind.hpp"
+#include "gazebo/gazebo.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include "stdio.h"
+#include <fstream>
+
+#include "gazebo/physics/PhysicsIface.hh"
+#include "gazebo/physics/Model.hh"
+#include "gazebo/physics/World.hh"
+#include "gazebo/physics/ContactManager.hh"
+#include "gazebo/physics/PhysicsEngine.hh"
+#include "gazebo/physics/Collision.hh"
+#include "gazebo/physics/Link.hh"
+#include <gazebo/physics/Inertial.hh>
+
+#include <gazebo/common/Plugin.hh>
+#include <ros/ros.h>
+#include "heca_plugins/Set_position.h"
+#include "heca_plugins/Set_activation.h"
+//#include <LinkConfig.hh>
+
+namespace gazebo
+{
+class positioner_plugin : public ModelPlugin
+{
+  public:
+	void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+	{
+
+		this->model = _parent;
+		this->world = this->model->GetWorld();
+		this->model_uav = this->world->GetModel("aeroarms_hecatonquiros_positioner_gripper_1");
+		this->number = this->world->GetModelCount();
+		this->nombres = this->world->GetModels();
+		this->nombre_mundo = this->world->GetName();
+		static_position = model_uav->GetLink("right/Positioner_Fija_BASE")->GetWorldPose();
+		//   this->jointR1_ = this->model->GetJoint("r1");
+		this->link_nombres = this->model_uav->GetLinks();
+		this->link_positioner = this->model_uav->GetLink("right/Positioner_Fija_BASE");
+		//   this->link_positioner->GetInertial()->SetMass(4); // Cambia la masa a 800000
+		//   this ->link_positioner ->UpdateMass();
+
+		archivo.open("Debugador_positioner.txt");
+		archivo << "\n"
+				<< this->model_uav->GetName()
+				<< "\t" << model->GetName()
+				<< "\t" << this->number
+				<< "\t" << this->link_nombres[1]->GetName()
+				<< std::endl;
+
+		std::cout << " \ntimee \n"<< std::endl;
+
+		//Server
+		if (!ros::isInitialized())
+		{
+			ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+							 << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+			return;
+		}
+
+		ROS_INFO("\n\nPluging activated!\n");
+		ros::NodeHandle n;
+		service = n.advertiseService("Set_position", &positioner_plugin::Model_pose, this);
+		autoservice = n.advertiseService("Set_autoposition", &positioner_plugin::Model_autopose, this);
+		activationservice = n.advertiseService("Set_activation", &positioner_plugin::activation, this);
+		ROS_INFO("Ready.");
+
+		this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&positioner_plugin::OnUpdate, this, _1));
+	}
+
+  public:
+	void OnUpdate(const common::UpdateInfo &_info)
+	{
+		if (set_activation == 1)
+		{	
+			//mover el dron entero
+			//math::Pose staticPose(posx,posy,posz,roll,pitch,yaw);
+			//this->model_uav->SetWorldPose(staticPose);
+			//Setlinkstatic
+			math::Pose staticPose(static_position);
+			this->link_positioner->SetWorldPose(staticPose);
+		}
+		else{
+			static_position = model_uav->GetLink("right/Positioner_Fija_BASE")->GetWorldPose();
+		}
+	}
+
+	bool Model_pose(heca_plugins::Set_position::Request &req,
+					heca_plugins::Set_position::Response &res)
+	{
+		posx = res.resu1 = req.x;
+		posy = res.resu2 = req.y;
+		posz = res.resu3 = req.z;
+		roll = res.resu4 = req.roll;
+		pitch = res.resu5 = req.pitch;
+		yaw = res.resu6 = req.yaw;
+		//ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
+		ROS_INFO("sending back response: [%ld,%ld,%ld,%ld,%ld,%ld]", (long int)res.resu1,
+				 (long int)res.resu2,
+				 (long int)res.resu3,
+				 (long int)res.resu4,
+				 (long int)res.resu5,
+				 (long int)res.resu6);
+
+		return true;
+	}
+
+	bool Model_autopose(heca_plugins::Set_position::Request &req,
+						heca_plugins::Set_position::Response &res)
+	{
+
+		res.resu1 = 10;
+		res.resu2 = 20;
+		res.resu3 = 30;
+		res.resu4 = 40;
+		res.resu5 = 50;
+		res.resu6 = 60;
+		ROS_INFO("sending auto back response: [%ld,%ld,%ld,%ld,%ld,%ld]", (long int)res.resu1,
+				 (long int)res.resu2,
+				 (long int)res.resu3,
+				 (long int)res.resu4,
+				 (long int)res.resu5,
+				 (long int)res.resu6);
+		return true;
+	}
+
+	bool activation(heca_plugins::Set_activation::Request &req,
+					heca_plugins::Set_activation::Response &res)
+	{
+		set_activation = res.resu = req.activation;
+		ROS_INFO("request: activation=%ld, ", (long int)req.activation);
+		ROS_INFO("sending back response: [%ld]", (long int)res.resu);
+		return true;
+	}
+
+  private:
+	physics::ModelPtr model;
+	physics::ModelPtr model_uav;
+	physics::WorldPtr world;
+	event::ConnectionPtr updateConnection;
+	std::ofstream archivo;
+	physics::JointPtr jointR1_;
+	physics::LinkPtr link_positioner;
+	physics::InertialPtr mass;
+	ignition::math::Pose3d Pos;
+	ros::ServiceServer service;
+	ros::ServiceServer autoservice;
+	ros::ServiceServer activationservice;
+	long int posx = 0, posy = 0, posz = 0, roll = 0, pitch = 0, yaw = 0;
+	unsigned int number;
+	physics::Model_V nombres;
+	physics::Link_V link_nombres;
+	std::string nombre_mundo;
+	long int set_activation = 0;
+	math::Pose static_position;
+};
+
+// Register this plugin with the simulator
+GZ_REGISTER_MODEL_PLUGIN(positioner_plugin)
+} // namespace gazebo
